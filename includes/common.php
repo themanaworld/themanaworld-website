a456780bfdfd87eb98d58cba0921cc3c37f0b7fc
Bjørn Lindeijer <bjorn@lindeijer.nl>
2006-07-25 08:57:16 +0000

    Fixed spelling to last news item, and committed the addition of Google Analytics

--- includes/common.php
+++ includes/common.php
@@ -151,6 +151,13 @@ function placeFooter()
     &copy; 2004-2005 The Mana World Dev Team
    </div>
   </div> <!-- /page -->
+
+  <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+  </script>
+  <script type="text/javascript">
+  _uacct = "UA-380618-3";
+  urchinTracker();
+  </script>
  </body>
 </html>
 <?php
