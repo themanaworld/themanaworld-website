6049f487a28d6d9ccf66817d35dcb7bdba3ba997
Bjørn Lindeijer <bjorn@lindeijer.nl>
2007-06-06 16:49:26 +0000

    Updated information about Debian package.

--- downloads.php
+++ downloads.php
@@ -10,7 +10,7 @@
 <div class="release"><img src="images/ico-src.png"/> <a href="http://prdownloads.sourceforge.net/themanaworld/tmw-0.0.23.tar.gz?download"><b>Source code</b> 0.0.23</a></div>
 <div class="release"><img src="images/ico-osx.png"/> <a href="http://prdownloads.sourceforge.net/themanaworld/tmw-0.0.23-osx.dmg?download"><b>MacOS X package</b> 0.0.23</a></div>
 <div class="release"><img src="images/ico-slackware.png"/> <a href="http://prdownloads.sourceforge.net/themanaworld/tmw-0.0.22.2-i686-1.tgz?download"><b>Slackware 11.0/Current package</b> 0.0.22.2</a></div>
-<div class="release"><img src="images/ico-deb.png"/> <a href="#repository"><b>Debian/Ubuntu repositories</b></a> (out of date)</div>
+<div class="release"><img src="images/ico-deb.png"/> <a href="http://packages.debian.org/tmw"><b>Debian package</b></a> (experimental currently)</div>
 <div class="release"><img src="images/ico-gentoo.png"/> <a href="http://packages.gentoo.org/packages/?category=games-rpg;name=tmw"><b>Gentoo eBuild</b></a></div>
 </div>
 
@@ -40,19 +40,12 @@ able to connect to eAthena, check out the <i>0.0</i> branch.
 </p>
 
 <a name="repository"></a>
-<h3>Debian/Ubuntu repositories</h3>
+<h3>Ubuntu repository (out of date)</h3>
 
-<p>For convenience to Debian and Ubuntu users, several package repositories
-have been set up by Bertram. The repositories also contain
+<p>For convenience for Ubuntu users, a package repository
+has been set up by Bertram. The repository also contains
 <a href="http://guichan.sourceforge.net/">Guichan</a>, because its latest
-version isn't generally available yet. Choose the right repository for your
-system, and add the given line to your sources.list file:</p>
-
-<h4>Debian Unstable</h4>
-
-<pre>
-deb http://bertram.ifrance.com/ debian unstable
-</pre>
+version isn't generally available yet.</p>
 
 <h4>Ubuntu Edgy (i386 and AMD-64)</h4>
 
