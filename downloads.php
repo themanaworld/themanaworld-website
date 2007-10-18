d9ba0c6c831154b5098cb3908cc0052c4f14065e
Bjørn Lindeijer <bjorn@lindeijer.nl>
2007-10-18 16:45:32 +0000

    Added Ubuntu package to the Downloads page, and commented out Ubuntu repository.

--- downloads.php
+++ downloads.php
@@ -5,15 +5,23 @@
 
 <h3>Current client release</h3>
 
+<table cellspacing="0" cellpadding="0" border="0">
+<tr><td>
 <div class="releases">
 <div class="release"><img src="images/ico-win.png"/> <a href="http://prdownloads.sourceforge.net/themanaworld/tmw-0.0.23-win32.exe?download"><b>Windows installer</b> 0.0.23</a></div>
 <div class="release"><img src="images/ico-src.png"/> <a href="http://prdownloads.sourceforge.net/themanaworld/tmw-0.0.23.tar.gz?download"><b>Source code</b> 0.0.23</a></div>
 <div class="release"><img src="images/ico-osx.png"/> <a href="http://prdownloads.sourceforge.net/themanaworld/tmw-0.0.23-osx.dmg?download"><b>MacOS X package</b> 0.0.23</a></div>
 <div class="release"><img src="images/ico-slackware.png"/> <a href="http://prdownloads.sourceforge.net/themanaworld/tmw-0.0.22.2-i686-1.tgz?download"><b>Slackware 11.0/Current package</b> 0.0.22.2</a></div>
+</div>
+</td><td>
+<div class="releases">
 <div class="release"><img src="images/ico-deb.png"/> <a href="http://packages.debian.org/tmw"><b>Debian package</b></a> (Debian unstable)</div>
+<div class="release"><img src="images/ico-ubuntu.png"/> <a href="http://packages.ubuntu.com/tmw"><b>Ubuntu package</b></a> (Gutsy Gibbon)</div>
 <div class="release"><img src="images/ico-gentoo.png"/> <a href="http://packages.gentoo.org/packages/?category=games-rpg;name=tmw"><b>Gentoo eBuild</b></a></div>
 <div class="release"><img src="images/ico-fedora.png"/> <a href="http://fedoraproject.org/wiki/Games/manaworld"><b>Fedora package</b></a></div>
 </div>
+</td></tr>
+</table>
 
 <p>For older versions please see our <a
 href="http://sourceforge.net/project/showfiles.php?group_id=106790">files
@@ -39,7 +47,7 @@ you're interested in testing the latest changes.</p>
 found in the <i>tmwserv</i> module. If you're interested in the latest version
 able to connect to eAthena, check out the <i>0.0</i> branch.
 </p>
-
+<!--
 <a name="repository"></a>
 <h3>Ubuntu repository (out of date)</h3>
 
@@ -58,7 +66,7 @@ deb http://bertram.ifrance.com/ ubuntu edgy
 
 <p><b>apt-get update<br/>
 apt-get install tmw</b></p>
-
+-->
 <?php
   placeFooter();
 ?>
