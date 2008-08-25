62aa564f73f0a1ef7fc6fda64212122d8f249a5d
Bjørn Lindeijer <bjorn@lindeijer.nl>
2008-08-25 12:07:26 +0000

    Patch by Jonathan Armani, adding links to OpenBSD and FreeBSD ports
    Since the amount of links became a bit big, Jonathan separated it in official
    packages and distribution packages.

--- downloads.php
+++ downloads.php
@@ -5,22 +5,31 @@
 
 <h3>Current client release</h3>
 
-<table cellspacing="0" cellpadding="0" border="0" style="border-width: 0px; margin-bottom: 0px;">
-<tr><td width="50%" style="border-width: 0px;">
-<div class="release"><img src="images/ico-win.png"/> <a href="http://downloads.sourceforge.net/themanaworld/tmw-0.0.25-win32.exe"><b>Windows installer</b> 0.0.25</a></div>
-<div class="release"><img src="images/ico-src.png"/> <a href="http://downloads.sourceforge.net/themanaworld/tmw-0.0.25.tar.gz"><b>Source code</b> 0.0.25</a></div>
-<div class="release"><img src="images/ico-osx.png"/> <a href="http://downloads.sourceforge.net/themanaworld/tmw-0.0.25-osx.dmg"><b>MacOS X package</b> 0.0.25</a></div>
-<div class="release"><img src="images/ico-slackware.png"/> <a href="http://downloads.sourceforge.net/themanaworld/tmw-0.0.25-i686-1.tgz"><b>Slackware 12.0 package</b> 0.0.25</a></div>
-</td><td width="50%" style="border-width: 0px;">
-<div class="release"><img src="images/ico-deb.png"/> <a href="http://packages.debian.org/tmw"><b>Debian package</b></a></div>
-<div class="release"><img src="images/ico-ubuntu.png"/> <a href="http://packages.ubuntu.com/tmw"><b>Ubuntu package</b></a></div>
-<div class="release"><img src="images/ico-gentoo.png"/> <a href="http://packages.gentoo.org/package/games-rpg/tmw"><b>Gentoo eBuild</b></a></div>
-<div class="release"><img src="images/ico-fedora.png"/> <a href="http://fedoraproject.org/wiki/Games/manaworld"><b>Fedora package</b></a></div>
+<p>Official packages:</p>
+<table style="border-width: 0px; width: 600px; margin-bottom: 0px; margin-top: 0px;" border="0" cellpadding="0" cellspacing="0">
+<tbody><tr><td style="border-width: 0px;" width="50%">
+<div class="release"><img src="images/ico-win.png"> <a href="http://downloads.sourceforge.net/themanaworld/tmw-0.0.25-win32.exe"><b>Windows installer</b> 0.0.25</a></div>
+<div class="release"><img src="images/ico-osx.png"> <a href="http://downloads.sourceforge.net/themanaworld/tmw-0.0.25-osx.dmg"><b>MacOS X package</b> 0.0.25</a></div>
+</td><td style="border-width: 0px;" width="50%">
+<div class="release"><img src="images/ico-src.png"> <a href="http://downloads.sourceforge.net/themanaworld/tmw-0.0.25.tar.gz"><b>Source code</b> 0.0.25</a></div>
+<div class="release"><img src="images/ico-slackware.png"> <a href="http://downloads.sourceforge.net/themanaworld/tmw-0.0.25-i686-1.tgz"><b>Slackware 12.0 package</b> 0.0.25</a></div>
 </td></tr>
-</table>
+</tbody></table>
 
-<p>For older versions please see our <a
-href="http://sourceforge.net/project/showfiles.php?group_id=106790">files
+<p>Distribution packages and ports:</p>
+<table style="border-width: 0px; width: 600px; margin-bottom: 0px; margin-top: 0px;" border="0" cellpadding="0" cellspacing="0">
+<tbody><tr><td style="border-width: 0px;" width="50%">
+<div class="release"><img src="images/ico-deb.png"> <a href="http://packages.debian.org/tmw"><b>Debian package</b></a></div>
+<div class="release"><img src="images/ico-ubuntu.png"> <a href="http://packages.ubuntu.com/tmw"><b>Ubuntu package</b></a></div>
+<div class="release"><img src="images/ico-fedora.png"> <a href="http://fedoraproject.org/wiki/Games/manaworld"><b>Fedora package</b></a></div>
+</td><td style="border-width: 0px;" width="50%">
+<div class="release"><img src="images/ico-gentoo.png"> <a href="http://packages.gentoo.org/package/games-rpg/tmw"><b>Gentoo eBuild</b></a></div>
+<div class="release"><img src="images/ico-freebsd.png"> <a href="http://www.freshports.org/games/tmw/"><b>FreeBSD port</b></a></div>
+<div class="release"><img src="images/ico-openbsd.png"> <a href="http://openports.se/games/tmw"><b>OpenBSD port</b></a></div>
+</td></tr>
+</tbody></table>
+
+<p>For older versions please see our <a href="http://sourceforge.net/project/showfiles.php?group_id=106790">files
 page</a> on SourceForge. Note however that they are no longer supported and
 likely to no longer work on our testing server.</p>
 
