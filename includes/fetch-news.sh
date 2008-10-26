0b3a6c8d74ef66861a8303af5284a577c37a9817
Bjørn Lindeijer <bjorn@lindeijer.nl>
2008-10-26 12:36:35 +0000

    Updated Slackware package download link and locations in fetch-news script

--- includes/fetch-news.sh
+++ includes/fetch-news.sh
@@ -3,6 +3,6 @@
 #    Fetches project news and saves it for local use.
 #
 
-/usr/bin/wget -q -O /home/groups/t/th/themanaworld/htdocs/includes/rss2_projnews.tmp 'http://sourceforge.net/export/rss2_projnews.php?group_id=106790&rss_fulltext=1' > /dev/null
-/bin/mv -f /home/groups/t/th/themanaworld/htdocs/includes/rss2_projnews.tmp \
-           /home/groups/t/th/themanaworld/htdocs/includes/rss2_projnews.cache
+/usr/bin/wget -q -O /home/tmw/public_html/www/includes/rss2_projnews.tmp 'http://sourceforge.net/export/rss2_projnews.php?group_id=106790&rss_fulltext=1' > /dev/null
+/bin/mv -f /home/tmw/public_html/www/includes/rss2_projnews.tmp \
+           /home/tmw/public_html/www/includes/rss2_projnews.cache
