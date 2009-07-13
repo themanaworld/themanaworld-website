b3c2517f0788106d10896b949e1f42fda86ac967
Bjørn Lindeijer <bjorn@lindeijer.nl>
2009-07-13 08:57:14 +0200

    Added servers page

---
+++ servers.php
@@ -0,0 +1,43 @@
+<?php
+  include("includes/common.php");
+  placeHeader("Servers");
+?>
+
+<p>There are multiple servers where you can play The Mana World. Each server
+can run a different world. Different servers may be set up for different target
+languages, but also as an entirely separate projects. Below is a non-exhaustive
+list of available servers.</p>
+
+<p style="background-color: #ede2da; padding: 5px; border: 1px solid
+#9f9894; -moz-border-radius: 10px;"><i>Security warning:</i> Do not use the same username and password on
+two different servers. The server admins can read all of them in clear text and
+nothing stops them from trying them on other servers. It happened a lot in the
+past that users of the official server got "hacked" because they ignored this
+important precaution.</p>
+
+<h3>Global The Mana World server (English)</h3>
+
+<p>Address: <b><tt>server.themanaworld.org</tt></b> (port 6901)<br/>
+<a href="http://server.themanaworld.org/">Online player list</a>, <a href="http://server.themanaworld.org/stats/">Statistics</a></p>
+
+<p>This is the official The Mana World server. The current client picks this as
+the default server. One thing to keep in mind is that since it's open to
+everybody, we ask you to speak English when around other people (other
+languages are fine in party or private chat, of course).</p>
+
+<h3>Brazilian The Mana World server (Portugese)</h3>
+
+<p>Address: <b><tt>server.themanaworld.com.br</tt></b> (port 6901)<br/>
+<a href="http://www.themanaworld.com.br/">Forums</a>, <a href="http://code.google.com/p/themanaworld-br/">Website</a></p>
+
+<p>Everybody is welcome to play on this server (though you should probably
+understand Portugese). It features a custom world developed separately from
+the content on the global server, based on the same graphics.</p>
+
+<h3>More alternative servers</h3>
+
+<p>More servers can be found on the wiki <a href="http://wiki.themanaworld.org/index.php/Servers">Servers</a> page. You may also advertise your own server there!</p>
+
+<?php
+  placeFooter();
+?>
