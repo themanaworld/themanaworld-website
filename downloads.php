fc53425ae893ddad104a5e265228cef20591a240
Jessica Tölke <jtoelke@mail.upb.de>
2013-05-09 13:21:32 +0200

    Updating the downloads page:
    - some phrasing fixes
    - distinct between mana and manaplus downloads
    - add downloads to some manaplus versions, especially the MAC versions

--- downloads.php
+++ downloads.php
@@ -8,13 +8,13 @@
 </div>
 
 <h3>ManaPlus client</h3>
-<p>The recommended client is available <a href="http://manaplus.evolonline.org/">on this other website</a>.</p>
+<p>The recommended client is available on the <a href="http://manaplus.evolonline.org/">ManaPlus website</a>.</p>
 
 <h3>Mana client</h3>
 
 <p>The previous official client for playing The Mana World is the Mana client
 and is developed as part of the <a href="http://manasource.org/">Mana
-project</a>. The latest version of the client can be downloaded from there.</p>
+project</a>. The latest version of this client can be downloaded from there.</p>
 
 <h3>The Mana World packages</h3>
 
@@ -25,8 +25,10 @@ project</a>. The latest version of the client can be downloaded from there.</p>
 <table style="border-width: 0px; width: 600px; margin-bottom: 0px; margin-top: 0px;" border="0" cellpadding="0" cellspacing="0">
 <tbody><tr><td style="border-width: 0px;" width="50%">
 
-<div class="release"><img src="images/ico-win.png"> <a href="http://sourceforge.net/projects/themanaworld/files/The%20Mana%20World/0.6.0/tmw-0.6.0-win32.exe/download"><b>Windows installer</b> 0.6.0</a></div>
-<div class="release"><img src="images/ico-osx.png"> <a href="http://server.themanaworld.org/mana-0.5.dmg"><b>MacOS X package</b> 0.5.0</a> (outdated, see forum)</div>
+<div class="release"><img src="images/ico-win.png"> <a href="http://download.evolonline.org/manaplus/download/manaplus-win32.exe"><b>ManaPlus Windows installer</b></a></div>
+<div class="release"><img src="images/ico-osx.png"> <a href="http://download3.evolonline.org/manaplus/macosx/manaplus-lion.dmg"><b>ManaPlus MacOS X package (lion)</b></a></div>
+<div class="release"><img src="images/ico-osx.png"> <a href="http://download3.evolonline.org/manaplus/macosx/manaplus-snowleopard.dmg"><b>ManaPlus MacOS X package (snow leopard)</b></a></div>
+<div class="release"><img src="images/ico-win.png"> <a href="http://sourceforge.net/projects/themanaworld/files/The%20Mana%20World/0.6.0/tmw-0.6.0-win32.exe/download"><b>Mana Windows installer</b> 0.6.0</a></div>
 <div class="release"><img src="images/ico-deb.png"> <a href="http://packages.debian.org/tmw"><b>Debian package</b></a></div>
 <div class="release"><img src="images/ico-ubuntu.png"> <a href="http://packages.ubuntu.com/tmw"><b>Ubuntu package</b></a></div>
 <div class="release"><img src="images/ico-fedora.png"> <a href="https://apps.fedoraproject.org/packages/tmw"><b>Fedora package</b></a></div>
@@ -35,8 +37,8 @@ project</a>. The latest version of the client can be downloaded from there.</p>
 
 <div class="release"><img src="images/ico-win.png"> <a href="http://portableapps.com/apps/games/the_mana_world_portable"><b>Windows Portable</b></a></div>
 <div class="release"><img src="images/ico-gentoo.png"> <a href="http://packages.gentoo.org/package/games-rpg/tmw"><b>Gentoo eBuild</b></a></div>
+<div class="release"><img src="images/ico-openbsd.png" width="16" height="16"> <a href="http://openports.se/games/manaplus"><b>OpenBSD port</b></a></div>
 <div class="release"><img src="images/ico-freebsd.png" width="16" height="16"> <a href="http://www.freshports.org/games/tmw/"><b>FreeBSD port</b></a></div>
-<div class="release"><img src="images/ico-openbsd.png" width="16" height="16"> <a href="http://openports.se/games/tmw"><b>OpenBSD port</b></a></div>
 
 </td></tr>
 </tbody></table>
@@ -50,7 +52,7 @@ project</a>. The latest version of the client can be downloaded from there.</p>
 </td></tr>
 </tbody></table>
 
-<p>For older versions please see our <a href="https://sourceforge.net/projects/themanaworld/files/">files
+<p>For older versions of Mana please see our <a href="https://sourceforge.net/projects/themanaworld/files/">files
 page</a> on SourceForge. Note however that these releases are no longer supported and
 likely to no longer work with our server.</p>
 
@@ -61,9 +63,19 @@ as it easily becomes out-of-date.</p>
 <p>To enable music in the client, make sure that "Download Music" is checked
 in the Audio tab of the Setup window.</p>
 
-<h3>Development version</h3>
+<h3>Development versions</h3>
 
-<p>The latest version of the client is developed as part of the
+<p>The latest version of the ManaPlus client is developed as part of the
+<a href="http://manaplus.evolonline.org/">ManaPlus project</a> and is available from the
+<a href="https://gitorious.org/manaplus/manaplus/">Git repositories on gitorious</a>. For
+checking out the latest development version of the client you can use the
+command:</p>
+
+<pre>
+git clone git://gitorious.org/manaplus/manaplus.git
+</pre>
+
+<p>The latest version of the Mana client is developed as part of the
 <a href="http://manasource.org/">Mana project</a> and is available from the
 <a href="http://github.com/mana">Git repositories on github</a>. For
 checking out the latest development version of the client you can use the
