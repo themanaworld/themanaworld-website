a2f0e5a32b1f7aef526621b7677c4d388b50e6a6
MadCamel <madcamel@gmail.com>
2014-03-02 13:08:34 -0500

    Updated About page with Wombat's suggestions

--- about.php
+++ about.php
@@ -4,28 +4,45 @@
 ?>
 
 <h3>Description</h3>
-
 <p>The Mana World (TMW) is a serious effort to create an innovative free and
 open source MMORPG. TMW uses 2D graphics and aims to create a large and diverse
 interactive world. It is licensed under the GPL, making sure this game can't
-ever run away from you.</p>
+ever run away from you.
 
-<p>The project includes the development of both a client and a server, as well
-as the development of an online world. At the moment we're making alpha
-releases of the client, while our server is in early development. The eAthena
-free software Ragnarok Online server is used until our own server has matured
-enough to replace it. Once ready, we'll be making releases of our server too so
-anybody will be free to set up their own server and start building their own
-online world.</p>
+
+<p>Explore this large, ever expanding world to defeat monsters, help NPCs and team up
+with friends as you achieve your goals. Get your weapons, armor and equipment
+through quests, monsters or crafting. Play mini-games, go on complex investigations
+or slay powerful bosses. Hang out in town, socialize or attend player organized events.
+
+Wear your boots and grab your sword, adventure waits for you!
+
+<h3>Story</h3>
+Start in the powerful city-state of Tulimshar before heading out into the vast expanses
+of the desert. Get powerful and explore the small continent of Argaes where magic is
+born. In The Mana World you are an adventurer and monster slayer defending the people
+of the world from the threats created during the Great Quake. Even in the icy heights of
+Nivalis there is a call for your assistance to keep the world safe and to grow your
+potential.
+
+<p>The Mage Council of Tulimshar has monitored events following the Great Quake and feel
+that something ominous is spreading throughout The Mana World. Monsters seem to of
+come out of every shadowy corner and petty dieties has begun to make presence in
+dark places. The council has made the call and you are just one of the many people
+that will battle the forces of evil, sending them back to the depths they came from.
+
+<p>Be it warrior, archer or mage, you have answered the call from the leaders of the
+world to fight back the darkness that spread after the Great Quake. Starting in
+the Tonori desert, you battle your way to forests surrounding Hurnscald or the icy
+mountains surrounding Nivalis. Monsters have nowhere to hide.
 
 <h3>Contributors</h3>
-
-<p>There are many people who have been involved in this project. For a list of
- people with developer access, check out the project
-<a href="http://sourceforge.net/project/memberlist.php?group_id=106790">
-member list</a>. On the wiki the
-<a href="http://wiki.themanaworld.org/index.php/Developers">Developers</a> page
-keeps track of the current contributors as well as retired contributors.</p>
+We are volunteer driven and encourage player participation in development. We have
+a long history of contributors. We try to make contributing to the game easy.
+<br>
+<br><a href="https://wiki.themanaworld.org/index.php/Joining_the_project">Get Involved!</a>
+<br><a href="https://wiki.themanaworld.org/index.php/Developers">Current Contributors</a>
+<br><a href="https://wiki.themanaworld.org/index.php/Contributors">Past Contributors</a>
 
 <?php
   placeFooter();
