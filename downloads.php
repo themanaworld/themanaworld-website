2d28c109fae339789da6f044e206104753fc8da1
wushin <pasekei@gmail.com>
2014-05-29 08:05:33 -0500

    Point at Manaplus on the Downloads page

--- downloads.php
+++ downloads.php
@@ -22,13 +22,13 @@
 <div class="release"><img src="images/ico-win.png"> <a href="http://download.evolonline.org/manaplus/tmw/tmw-win32.exe"><b>Windows installer (ManaPlus)</b></a></div>
 <div class="release"><img src="images/ico-osx.png"> <a href="http://download3.evolonline.org/manaplus/macosx/manaplus-lion.dmg"><b>ManaPlus MacOS X package (lion)</b></a></div>
 <div class="release"><img src="images/ico-osx.png"> <a href="http://download3.evolonline.org/manaplus/macosx/manaplus-snowleopard.dmg"><b>ManaPlus MacOS X package (snow leopard)</b></a></div>
-<div class="release"><img src="images/ico-deb.png"> <a href="http://packages.debian.org/tmw"><b>Debian package</b></a></div>
-<div class="release"><img src="images/ico-ubuntu.png"> <a href="http://packages.ubuntu.com/tmw"><b>Ubuntu package</b></a></div>
-<div class="release"><img src="images/ico-fedora.png"> <a href="https://apps.fedoraproject.org/packages/tmw"><b>Fedora package</b></a></div>
-<div class="release"><img src="images/ico-android.png"> <a href="http://download.evolonline.org/manaplus/download/manaplus.apk"><b>Android App (ManaPlus)</b></a></div>
-<div class="release"><img src="images/ico-gentoo.png"> <a href="http://packages.gentoo.org/package/games-rpg/tmw"><b>Gentoo eBuild</b></a></div>
+<div class="release"><img src="images/ico-deb.png"> <a href="http://packages.debian.org/manaplus"><b>Debian package</b></a></div>
+<div class="release"><img src="images/ico-ubuntu.png"> <a href="http://packages.ubuntu.com/manaplus"><b>Ubuntu package</b></a></div>
+<div class="release"><img src="images/ico-fedora.png"> <a href="https://apps.fedoraproject.org/packages/manaplus"><b>Fedora package</b></a></div>
+<div class="release"><img src="images/ico-android.png"> <a href="https://play.google.com/store/apps/details?id=org.evolonline.beta.manaplus"><b>Android App (ManaPlus)</b></a></div>
+<div class="release"><img src="images/ico-gentoo.png"> <a href="http://packages.gentoo.org/package/games-rpg/manaplus"><b>Gentoo eBuild</b></a></div>
 <div class="release"><img src="images/ico-openbsd.png" width="16" height="16"> <a href="http://openports.se/games/manaplus"><b>OpenBSD port</b></a></div>
-<div class="release"><img src="images/ico-freebsd.png" width="16" height="16"> <a href="http://www.freshports.org/games/tmw/"><b>FreeBSD port</b></a></div>
+<div class="release"><img src="images/ico-freebsd.png" width="16" height="16"> <a href="http://www.freshports.org/games/manaplus/"><b>FreeBSD port</b></a></div>
 
 </td></tr>
 </tbody></table>
