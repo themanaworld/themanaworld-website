eecbbfe1bec9553811d5429e62ef9550bb763e43
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-10-15 01:17:22 -0430

    :art: improve arrays coding style.

--- extensions/tmwa/frontend/language/TMWAccount.alias.php
+++ extensions/tmwa/frontend/language/TMWAccount.alias.php
@@ -6,10 +6,14 @@
  * @ingroup Extensions
  */
 
-$specialPageAliases = array();
-
-/** English (English) */
-$specialPageAliases['en'] = array(
-	'GameAccountPage' => array( 'The Mana World Account' ),
-	'GameNewsPage' => array( 'The Mana World News' )
+$specialPageAliases = array(
+    /** English (English) */
+    'en' => array(
+        'GameAccountPage' => array(
+            'The Mana World Account'
+        ),
+        'GameNewsPage'    => array(
+            'The Mana World News'
+        )
+    ),
 );
--- extensions/tmwa/frontend/language/TMWAccount.i18n.php
+++ extensions/tmwa/frontend/language/TMWAccount.i18n.php
@@ -6,10 +6,10 @@
  * @ingroup Extensions
  */
 
-$messages = array();
-
-$messages['en'] = array(
-	'gameaccount'            => 'The Mana World Game Account',
-	'gamenews'            => 'The Mana World Game News',
-	'gameaccount-loginnotice' => 'To obtain a game account, you must register here: \'\'\'[[Special:GameAccount|Game Account]]\'\'\'.<br/> To obtain a wiki account, you must register here: \'\'\'[[Special:RequestAccount|Wiki Account]]\'\'\'.',
+$messages = array(
+    'en' => array(
+        'gameaccount'             => 'The Mana World Game Account',
+        'gamenews'                => 'The Mana World Game News',
+        'gameaccount-loginnotice' => 'To obtain a game account, you must register here: \'\'\'[[Special:GameAccount|Game Account]]\'\'\'.<br/> To obtain a wiki account, you must register here: \'\'\'[[Special:RequestAccount|Wiki Account]]\'\'\'.',
+    ),
 );
