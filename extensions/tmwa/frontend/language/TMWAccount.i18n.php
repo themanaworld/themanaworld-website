f942517b9b95cb046fc85dd6b9c2785ddada7c52
wushin <pasekei@gmail.com>
2016-03-14 00:06:39 -0500

    Fix long page titles

--- extensions/tmwa/frontend/language/TMWAccount.i18n.php
+++ extensions/tmwa/frontend/language/TMWAccount.i18n.php
@@ -8,14 +8,14 @@
 
 $messages = array(
     'en' => array(
-        'gameaccount'             => 'The Mana World Game Account',
-        'gamenews'                => 'The Mana World Game News',
+        'gameaccount'             => 'Game Account',
+        'gamenews'                => 'Game News',
         'gameaccount-loginnotice' => 'To obtain a game account, you must register here: \'\'\'[[Special:GameAccount|Game Account]]\'\'\'.<br/> To obtain a wiki account, you must register here: \'\'\'[[Special:RequestAccount|Wiki Account]]\'\'\'.',
     ),
 
     'es' => array(
-        'gameaccount'             => 'Cuenta de Juego en The Mana World',
-        'gamenews'                => 'Noticias del Juego The Mana World',
+        'gameaccount'             => 'Cuenta de Juego',
+        'gamenews'                => 'Noticias del Juego',
         'gameaccount-loginnotice' => 'Para obtener una cuenta de juego, debes registrarte aquí: \'\'\'[[Special:GameAccount|Game Account]]\'\'\'.<br/> Para obtener una cuenta en la wiki, debes registrarte aquí: \'\'\'[[Special:RequestAccount|Wiki Account]]\'\'\'.',
     ),
 );
