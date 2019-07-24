276896d9a6f187b486de46bc59a8be0226912985
gumi <git@gumi.ca>
2019-07-24 12:42:38 -0400

    add normalize.css

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import VS2 from "vue-script2"
+import "normalize.css"
 
 Vue.config.productionTip = false
 Vue.use(VS2)
