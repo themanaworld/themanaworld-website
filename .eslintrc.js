c3d48e0be54364cf14df4eb1c3ed49d74ae8ab6d
gumi <git@gumi.ca>
2021-08-19 10:34:14 -0400

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,15 @@ module.exports = {
 		node: true
 	},
 	'extends': [
-		'plugin:vue/vue3-essential',
-		'eslint:recommended',
-		'@vue/typescript/recommended'
-	],
+      'plugin:vue/vue3-essential',
+      'eslint:recommended',
+      '@vue/typescript/recommended',
+      '@vue/typescript'
+    ],
 	parserOptions: {
-		ecmaVersion: 2020
-	},
+      ecmaVersion: 2020,
+      parser: '@typescript-eslint/parser'
+    },
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,6 @@
-declare module "*.vue" {
-	import type { DefineComponent } from "vue"
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const component: DefineComponent<{}, {}, any>
-	export default component
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
