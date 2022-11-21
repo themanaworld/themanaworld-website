1297be11f5a2edcd3437e37be2607e603a8e6d14
Jesusaves <jesusalva@themanaworld.org>
2022-11-21 08:39:49 -0300

    Update link

--- src/views/NewsArchive.vue
+++ src/views/NewsArchive.vue
@@ -2,7 +2,7 @@
 <template>
 	<main class="main-content">
 		<h1>News archive</h1>
-		<a href="https://themanaworld.github.io/tmwa-server-data/news.rss" target="_blank" rel="noopener" title="RSS feed" aria-label="open the RSS feed" class="feed"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg></a>
+		<a href="https://updates.tmw2.org/legacy/news.rss" target="_blank" rel="noopener" title="RSS feed" aria-label="open the RSS feed" class="feed"><svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 5c7.18 0 13 5.82 13 13M6 11a7 7 0 017 7m-6 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg></a>
 		<News :count="Infinity"/>
 	</main>
 </template>
